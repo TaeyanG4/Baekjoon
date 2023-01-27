43ca875434c932478f6da0be0631d104bd0d03fa
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-01-27 23:44:14 +0900

    [Bronze II] Title: 분해합, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,13 +2,11 @@ import sys
 input = sys.stdin.readline
 
 def sol(v):
-    cnt = v // 2 
-    while True:
-        if v == cnt + sum(map(int, str(cnt))): 
-            return cnt
-        cnt += 1
-        if cnt > v: 
-            return 0
+    for i in range(len(str(v))*9, 0, -1):
+        dis = abs(v-i)
+        if v==dis+sum(map(int, str(dis))):
+            return dis
+    return 0
 
 if __name__ == '__main__':
         print(sol(int(input())))
