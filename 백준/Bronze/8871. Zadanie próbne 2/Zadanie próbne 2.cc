fc0b9fe8a02f46aa911d0e6906d33f6b31e707a1
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-03 17:27:57 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    int val;
+    std::cin>>val;
+    std::cout<<2*(val+1)<<" "<<3*(val+1)<<std::endl;
+}
