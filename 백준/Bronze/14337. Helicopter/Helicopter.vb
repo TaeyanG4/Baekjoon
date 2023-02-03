4bc039dda96a335aa22c1c95c6f5635bdceb0894
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-03 17:53:44 +0900

    [Bronze V] Title: Helicopter, Time: 60 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("      ===================")
+        Console.WriteLine("          ____||___")
+        Console.WriteLine("\ /      /       []\")
+        Console.WriteLine(" X=======           \__")
+        Console.WriteLine("/ \      \____________|")
+        Console.WriteLine("            ||  ||")
+        Console.WriteLine("         \-----------/")
+        Console.ReadLine()
+    End Sub
+
+End Module
