fa9b2eeb17f8753bde6dee075f964bc576218f2f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-03 18:11:43 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        cout << "SciComLove" << endl;
+    }
+}
