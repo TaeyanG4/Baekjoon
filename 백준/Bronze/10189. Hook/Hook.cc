15e46bf68b07846ec670e7acbf5c190ba4b968a6
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-03 17:51:29 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    std::cout << "#  # #### #### #  #" << std::endl;
+    std::cout << "#### #  # #  # # #" << std::endl;
+    std::cout << "#### #  # #  # # #" << std::endl;
+    std::cout << "#  # #### #### #  #" << std::endl;
+}
