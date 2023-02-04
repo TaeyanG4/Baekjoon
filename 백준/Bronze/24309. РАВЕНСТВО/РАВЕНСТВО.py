2aaf69f7b9612a08723cc7afa39ad9468fa67529
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-04 15:42:37 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 272 ms, Memory: 50376 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.py
@@ -0,0 +1,24 @@
+# import line
+#################################
+import sys
+import math
+import copy
+import ast
+import re
+import statistics as st
+from decimal import *
+from collections import *
+from itertools import *
+from heapq import *
+#################################
+
+def sol(n):
+    pass
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    
+    # Input
+    a, b, c = int(input()), int(input()), int(input())
+    
+    # Output
+    print(int((b-c)//a))
