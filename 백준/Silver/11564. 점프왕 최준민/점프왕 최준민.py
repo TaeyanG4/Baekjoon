64a6a39653a1f6329953047adf6f5686ffdc8230
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-13 18:36:56 +0900

    [Silver V] Title: 점프왕 최준민, Time: 196 ms, Memory: 50748 KB -BaekjoonHub

---
+++ 백준/Silver/11564. 점프왕 최준민/점프왕 최준민.py
@@ -0,0 +1,30 @@
+# import lines
+#################################
+import sys
+import math
+import copy
+import ast
+import re
+import statistics as st
+from decimal import *
+from collections import *
+from itertools import *
+from heapq import *
+#################################
+
+def sol(k, a, b):
+    start = a + (k - a % k) % k
+    end = b - b % k
+
+    cnt = (end - start) // k + 1
+    
+    return cnt
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    
+    # Input
+    k, a, b = map(int, input().split())
+    
+    # Output
+    print(sol(k, a, b))
