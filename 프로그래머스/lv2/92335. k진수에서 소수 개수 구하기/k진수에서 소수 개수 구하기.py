6e01e002f1991fc88899428a00fdacc297effed8
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-16 13:09:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,26 +1,24 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
+# n을 k진법으로 나타낸 문자열 반환
+def conv(n, k):
+    s = ''
+    while n:
+        s += str(n%k)
+        n //= k
+    return s[::-1]
+
+# n이 소수인지 판정
+def isprime(n):
+    if n <= 1: return False
+    i = 2
+    while i*i <= n:
+        if n%i == 0: return False
+        i += 1
     return True
 
 def solution(n, k):
+    s = conv(n,k)
     cnt = 0
-    num = []
-
-    # n을 k진수로 변환
-    while n > 0:
-        num.append(n % k)
-        n //= k
-    
-    # 변환된 수를 0을 기준으로 split함
-    numbers = ''.join(str(i) for i in list(reversed(num))).split('0')
-    
-    # 소수 판별
-    for sosu in numbers:
-        if sosu != ''and is_prime(int(sosu)):
-                cnt += 1
-
+    for num in s.split('0'):
+        if not num: continue # 빈 문자열에 대한 예외처리
+        if isprime(int(num)): cnt += 1
     return cnt
