e7000417ed1c6f16ec74838ffcf443cbe977984c
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-16 14:41:19 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,25 +1,29 @@
-def isitPrime(k):
-    if k==2 or k==3: return True
-    if k%2==0 or k<2: return False
-    for i in range(3, int(k**0.5)+1, 2):
-        if k%i==0:
+# 소수 판별 함수
+def is_prime(n):
+    if n == 2:
+        return True
+    if n < 2:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
             return False
-
     return True
 
 def solution(n, k):
-    answer = 0
-    num_str = ""
+    cnt = 0
+    num = []
 
+    # n을 k진수로 변환
     while n > 0:
-        num_str = str(n%k) + num_str
-        n = n//k
-
-
-    arr = num_str.split('0')
-    for i in arr:
-        if i != '':
-            if isitPrime(int(i)):
-                answer+=1
-
-    return answer
+        num.append(n % k)
+        n //= k
+    
+    # 변환된 수를 0을 기준으로 split함
+    numbers = ''.join(str(i) for i in list(reversed(num))).split('0')
+    numbers = [x for x in numbers if x != '']
+    
+    # 소수 판별
+    for sosu in numbers:
+        if is_prime(int(sosu)):
+                cnt += 1
+    return cnt
