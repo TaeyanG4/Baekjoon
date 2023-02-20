47ccf878caf0f41589e270dad06558fe78610f71
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-02-20 22:22:34 +0900

    [Silver IV] Title: Router, Time: 232 ms, Memory: 52756 KB -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.py
@@ -0,0 +1,41 @@
+# import lines
+#################################
+import sys
+import math
+import copy
+import ast
+import re
+import time
+import json
+import statistics as st
+from decimal import *
+from collections import *
+from itertools import *
+from heapq import *
+#################################
+
+# 0 = queue , 1 = stack
+def sol(buffer):
+    ans = deque()
+    while True:
+        v = int(input())
+        if v == -1:
+            break
+        if v is 0:
+            ans.popleft()
+        elif len(ans) <= buffer and v is not -1:
+            ans.append(v)
+    
+    if len(ans) is 0:
+        return "empty"
+    else:
+        return list(ans)
+    
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    
+    # input
+    buffer = int(input())
+
+    # output
+    print(*sol(buffer))
