a54e361af1a6f292e284a1af6783d6a95acc2695
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-04-03 14:39:56 +0900

    [Bronze IV] Title: 입실 관리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.py
@@ -0,0 +1,18 @@
+# import lines
+#################################
+import sys
+#################################
+
+def solution(n, s):
+    pass
+    
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    # input
+    t = int(input())
+    
+    # output
+    for _ in range(t):
+        n = input().strip()
+        print(n.lower())
