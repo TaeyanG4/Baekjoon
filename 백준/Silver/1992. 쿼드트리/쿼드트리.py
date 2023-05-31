daf76c3484c2b7af2e9a87ac7d2af16dd5376947
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-05-31 15:20:35 +0900

    [Silver I] Title: 쿼드트리, Time: 64 ms, Memory: 36300 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -0,0 +1,46 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# import statistics as st
+# from decimal import *
+from collections import *
+# from itertools import *
+# from heapq import *
+# from datetime import datetime
+#################################
+
+def conquest(n, mat, i, j, size):
+    if size == 1:
+        return str(mat[i][j])
+    
+    half = size // 2
+    a = conquest(n, mat, i, j, half)
+    b = conquest(n, mat, i, j+half, half)
+    c = conquest(n, mat, i+half, j, half)
+    d = conquest(n, mat, i+half, j+half, half)
+    
+    if a == b == c == d == '0' or a == b == c == d == '1':
+        return a
+    else:
+        return '(' + a + b + c + d + ')'
+
+def solution(n, mat):
+    return conquest(n, mat, 0, 0, n)
+    
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    # input
+    n = int(input())
+    mat = [list(map(int, input().strip())) for _ in range(n)] 
+    
+    # output
+    print(solution(n, mat))
+    
