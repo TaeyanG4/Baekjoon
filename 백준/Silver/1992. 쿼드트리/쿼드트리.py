9bf09de4c37689d39dbaf937cac4812b01536b6e
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-05-31 15:29:12 +0900

    [Silver I] Title: 쿼드트리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,35 +1,20 @@
 # import lines
-#################################
 import sys
-import math
-# import copy
-# import ast
-# import re
-# import time
-# import json
-# import pprint
-# import statistics as st
-# from decimal import *
-from collections import *
-# from itertools import *
-# from heapq import *
-# from datetime import datetime
-#################################
 
 def conquest(n, mat, i, j, size):
     if size == 1:
         return mat[i][j]
     
     half = size // 2
-    a = conquest(n, mat, i, j, half)
-    b = conquest(n, mat, i, j+half, half)
-    c = conquest(n, mat, i+half, j, half)
-    d = conquest(n, mat, i+half, j+half, half)
+    lu = conquest(n, mat, i, j, half)
+    ru = conquest(n, mat, i, j+half, half)
+    ld = conquest(n, mat, i+half, j, half)
+    rd = conquest(n, mat, i+half, j+half, half)
     
-    if a == b == c == d == '0' or a == b == c == d == '1':
-        return a
+    if lu == ru == ld == rd == '0' or lu == ru == ld == rd == '1':
+        return lu
     else:
-        return '(' + a + b + c + d + ')'
+        return '('+lu+ru+ld+rd+')'
 
 def solution(n, mat):
     return conquest(n, mat, 0, 0, n)
@@ -43,4 +28,3 @@ if __name__ == '__main__':
     
     # output
     print(solution(n, mat))
-    
