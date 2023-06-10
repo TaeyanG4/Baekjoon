fc73d6d8a920f7e2bd629138eaf134b2f93875f3
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-06-10 23:26:17 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -0,0 +1,28 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from itertools import *
+# from heapq import *
+# from datetime import datetime
+#################################
+
+
+def solution():
+    return ((2**n) + 1) ** 2
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    # input
+    n = int(input())
+
+    # output
+    print(solution())
