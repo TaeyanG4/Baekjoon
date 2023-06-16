0a0a8ed8e0a9e36db30d29239837c1492d3789e1
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-06-16 22:02:04 +0900

    [Gold II] Title: 피보나치 수 6, Time: 168 ms, Memory: 115532 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -19,8 +19,7 @@ def square(mat1, mat2):
     for i in range(2):
         for j in range(2):
             for k in range(2):
-                res[i][j] += mat1[i][k] * mat2[k][j]
-            res[i][j] %= 1000000007
+                res[i][j] += mat1[i][k] * mat2[k][j] % 1000000007
     return res
 
 def solution(mat, n):
