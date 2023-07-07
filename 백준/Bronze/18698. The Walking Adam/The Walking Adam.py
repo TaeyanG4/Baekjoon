7dd263a23d3c58a9236b6042ef8680e675380962
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-07 12:20:36 +0900

    [Bronze IV] Title: The Walking Adam, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/18698. The Walking Adam/The Walking Adam.py
@@ -0,0 +1,36 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from itertools import *
+# from heapq import *
+# from datetime import datetime
+# from bisect import *
+# from statistics import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # sys.setrecursionlimit(10**6)
+    
+    # input
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ans = 0
+        for c in s:
+            if c == 'U':
+                ans += 1
+            else:
+                break
+        print(ans)
