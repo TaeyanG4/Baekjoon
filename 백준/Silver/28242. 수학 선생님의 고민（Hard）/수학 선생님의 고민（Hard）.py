a63304e8501dcf94aa9e2a6632c6e8c48ce3102d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-14 00:42:17 +0900

    [Silver I] Title: 수학 선생님의 고민(Hard), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/28242. 수학 선생님의 고민（Hard）/수학 선생님의 고민（Hard）.py
+++ 백준/Silver/28242. 수학 선생님의 고민（Hard）/수학 선생님의 고민（Hard）.py
@@ -10,7 +10,7 @@ import sys
 # import time
 # import pprint
 # from collections import *
-# from itertools import *
+from itertools import *
 # from statistics import *
 # from heapq import *
 # from datetime import datetime
@@ -20,20 +20,19 @@ import sys
 def solution(a, b, c):
     
     # 판별식이 음수면 해가 없다
-    if b ** 2 - 4 * a * c < 0:
+    if b**2 - 4*a*-c < 0:
         return -1
     
     # 모든 a와 c의 약수 조합을 찾는다.
     # 제곱근까지만 찾아도 된다
-    factors_a = [(i, a//i) for i in range(1, abs(a)+1) if a % i == 0]
-    factors_c = [(i, c//i) for i in range(1, abs(c)+1) if c % i == 0]
-    
-    # 모든 약수 조합에 대해
-    for fa in factors_a:
-        for fc in factors_c:
-            xa, xb, xc, xd = fa[0], fc[0], fa[1], fc[1]
-            if xa * xc == a and xb * xd == c and xa * xd + xb * xc == b:
-                return xa, xb, xc, xd
+    factors_a = [(i, a//i) for i in range(1, int(abs(a)**0.5)+1) if a % i == 0]
+    factors_c = [(i, c//i) for i in range(1, int(abs(c)**0.5)+1) if c % i == 0]
+
+    # 모든 약수 조합에 대해 b를 만족하는지 확인한다.
+    for (fa, fc), (fb_, fd_) in product(factors_a, factors_c):
+        for fb, fd in (fb_, -fd_), (-fb_, fd_):
+            if fa * fd + fb * fc == b:
+                return fa, fb, fc, fd
     return -1
 
 if __name__ == '__main__':
@@ -45,7 +44,7 @@ if __name__ == '__main__':
     n = int(input())
     
     # output
-    ans = solution(n, n+1, -(n+2))
+    ans = solution(n, n+1, n+2)
     if ans == -1:
         print(-1)
     else:
