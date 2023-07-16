7fcedcaf7f050ebba2567a1604df793202dcd16a
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-16 23:07:41 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,24 @@
+def solution(s):
+    ans = []
+    
+    if len(s) == 1:
+        return 1
+    
+    for i in range(1, len(s)+1):
+        b = ''
+        cnt = 1
+        temp = s[:i]
+        for j in range(i, len(s)+i, i):
+            
+            if temp == s[j:j+i]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    b = b + str(cnt) + temp
+                else:
+                    b = b + temp
+                    
+                temp = s[j:j+i]
+                cnt = 1
+        ans.append(len(b))
+    return min(ans)
