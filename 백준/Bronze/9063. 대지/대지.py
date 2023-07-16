857d6845997b42776d23c5e747f07cc3a7699d52
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-17 03:37:58 +0900

    [Bronze III] Title: 대지, Time: 92 ms, Memory: 35196 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,35 +1,11 @@
-# import line
-#################################
 import sys
-import math
-import copy
-import ast
-import re
-import statistics as st
-from decimal import *
-from collections import *
-from itertools import *
-from heapq import *
-#################################
+input = sys.stdin.readline
 
-def solution(n, x_arr, y_arr):
-    if n == 1:
-        return 0
-    else:
-        x_val = max(x_arr) - min(x_arr)
-        y_bal = max(y_arr) - min(y_arr)
-        return x_val * y_bal
-
-if __name__ == '__main__':
-    # input = sys.stdin.readline
-    
-    # input
-    n = int(input())
-    x_arr, y_arr = list(), list()
-    for i in range(n):
-        x, y = map(int, input().split())
-        x_arr.append(x)
-        y_arr.append(y)
-    
-    # output
-    print(solution(n, x_arr, y_arr))
+B = []
+C = []
+a = int(input())
+for i in range(a):
+    b,c = map(int, input().split())
+    B.append(b)
+    C.append(c)
+print((max(B)-min(B))*(max(C)-min(C)))
