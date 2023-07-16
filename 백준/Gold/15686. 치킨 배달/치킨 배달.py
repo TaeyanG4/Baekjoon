eb39573add4988862f1fc8d52be3a91507779aa6
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-16 20:06:44 +0900

    [Gold V] Title: 치킨 배달, Time: 364 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,50 +2,55 @@
 #################################
 import sys
 import math
-import copy
+# import copy
 # import ast
 # import re
 # import time
 # import json
+# import time
 # import pprint
-from collections import *
-from heapq import *
+# from collections import *
+# from heapq import *
 from itertools import *
+# from statistics import *
 # from datetime import datetime
+# from bisect import *
 #################################
 
-def distance(house_li, chicken_li):
+def distance(houses_li, chiken_li):
     min_val = 0
-    for h in house_li:
-        temp = sys.maxsize
-        for c in chicken_li:
-            temp = min(temp, abs(c[0]-h[0])+abs(c[1]-h[1]))
+    for house in houses_li:
+        temp = INF
+        for chicken in chiken_li:
+            temp = min(temp, abs(house[0]-chicken[0]) + abs(house[1]-chicken[1]))
         min_val += temp
     return min_val
-
-def solution(cnt):
-    global dist
-    for v in combinations(chicken, m):
-        dist = min(dist, distance(house, v))
+    
+def solution(houses, chikens):
+    global min_dist
+    for chiken_m in combinations(chikens, m):
+        min_dist = min(min_dist, distance(houses, chiken_m))
 
 if __name__ == '__main__':
     input = sys.stdin.readline
-    # sys.setrecursionlimit(10**5)
+    INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
     # input
-    n, m = map(int, input().split())
-    li = [list(map(int, input().split())) for _ in range(n)]
-    dist = sys.maxsize
+    n, m  = map(int, input().split())
+    mat = [list(map(int, input().split())) for _ in range(n)]
     
-    chicken, house = [], []
+    houses, chikens = [], []
     for i in range(n):
         for j in range(n):
-            if li[i][j] == 2:
-                chicken.append((i, j))
-            if li[i][j] == 1:
-                house.append((i, j))
+            if mat[i][j] == 1:
+                houses.append((i, j))
+            elif mat[i][j] == 2:
+                chikens.append((i, j))
+    
+    min_dist = INF
     
     # output
-    solution(0)
-    print(dist)
-    
+    solution(houses, chikens)
+    print(min_dist)
