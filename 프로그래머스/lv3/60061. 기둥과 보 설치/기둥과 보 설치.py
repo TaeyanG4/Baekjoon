dccb2267fa5f4004d2e14fe7c380bfa44d26e20b
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-16 23:52:19 +0900

    [level 3] Title: 기둥과 보 설치, Time: 749.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -0,0 +1,47 @@
+def buildable(x, y, a, res):
+    
+    # 기둥
+    if a == 0:
+        # 바닥이거나 기둥이 있는 경우
+        if y == 0 or (x, y - 1, 0) in res: 
+            return True
+        
+        # 바닥에 보가 있는 경우
+        if (x - 1, y, 1) in res: 
+            return True
+        
+        if (x, y, 1) in res: 
+            return True
+    
+    # 보
+    else:
+        # 양끝에 보가 있는경우
+        if (x - 1, y, 1) in res and (x + 1, y, 1) in res: 
+            return True
+        
+        # 한쪽 끝에 기둥이 있는경우
+        if (x + 1, y - 1, 0) in res or (x, y - 1, 0) in res: 
+            return True
+    
+    return False
+
+def solution(n, build_frame):
+    res = []
+    
+    for x, y, a, b in build_frame:
+        
+        # bulid
+        if b:
+            if buildable(x, y, a, res):
+                res.append((x, y, a))
+        
+        # remove
+        else:
+            res.remove((x, y, a))
+            for nx, ny, na in res:
+                if not buildable(nx, ny, na, res):
+                    res.append((x, y, a))
+                    break
+    
+    res.sort()
+    return res
