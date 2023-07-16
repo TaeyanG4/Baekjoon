8ddb80293b327b35c5bfa0b7e0051df363cfe39b
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-16 15:57:55 +0900

    [Gold II] Title: 공항, Time: 124 ms, Memory: 37232 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.py
@@ -0,0 +1,53 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def find(x):
+    if alters[x] == x:
+        return x
+    else:
+        alters[x] = find(alters[x])
+        return alters[x]
+
+def union(root):
+    alters[root] = find(root - 1)
+
+def solution(g, p):
+    
+    cnt = 0
+    for i in range(p):
+        root = find(int(input()))
+        if root == 0:
+            break
+        union(root)
+        cnt += 1
+    return cnt
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    g = int(input())
+    p = int(input())
+    alters = list(range(g + 1))
+    
+    # output
+    print(solution(g, p))
