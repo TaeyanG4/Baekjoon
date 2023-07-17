68557ff24c9f6b72c684641e6ed3c41525b78ed3
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-18 03:37:43 +0900

    [Gold V] Title: 무한 수열, Time: 72 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -0,0 +1,44 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def dfs(n):
+    if n in dp:
+        return dp[n]
+    
+    if n == 0:
+        return 1
+    
+    dp[n] = dfs(n//p) + dfs(n//q)
+    
+    return dp[n]
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n, p, q = map(int, input().split())
+    dp = defaultdict(int)
+    dp[0] = 1
+    
+    # output
+    dfs(n)
+    print(dp[n])
