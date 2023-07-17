afa230e9892f54cc4ae13f91fdd8e351d2956c96
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-18 03:45:22 +0900

    [Gold V] Title: 무한 수열, Time: 64 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -18,11 +18,11 @@ from collections import *
 #################################
 
 def dfs(n):
-    if n in dp:
-        return dp[n]
-    
     if n == 0:
         return 1
+    
+    if n in dp:
+        return dp[n]
     
     dp[n] = dfs(n//p) + dfs(n//q)
     
