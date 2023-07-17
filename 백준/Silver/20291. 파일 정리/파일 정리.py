e047800c2fec5067a0078640ffeca66a477860f0
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-18 02:14:40 +0900

    [Silver III] Title: 파일 정리, Time: 204 ms, Memory: 59396 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -0,0 +1,39 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution(n):
+    
+    dic = defaultdict(int)
+    for i in range(n):
+        name, ext = input().rstrip().split('.')
+        dic[ext] += 1
+        
+    return '\n'.join([f'{k} {v}' for k, v in sorted(dic.items())])
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    
+    # output
+    print(solution(n))
