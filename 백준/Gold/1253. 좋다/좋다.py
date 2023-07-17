3402893bea609250a5967407d71ebc7a86cfde29
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-18 04:44:46 +0900

    [Gold IV] Title: 좋다, Time: 400 ms, Memory: 36228 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -18,40 +18,23 @@ from collections import *
 #################################
 
 def solution(n, li):
-    
-    # 좋은 수의 개수
     ans = 0
-    
-    # 각 숫자가 몇 번 나왔는지 세기
-    dic = defaultdict(int)
-    for i in li:
-        dic[i] += 1
+    for idx, num in enumerate(li):
         
-    # 좋은 수 세기
-    for i in range(n):
-        for j in range(i+1, n):
-            temp = li[i] + li[j]
-            
-            # 두 수 모두 합한 수와 같을 경우 (0 + 0 = 0)
-            if temp == li[i] and temp == li[j]:
-                # 3개 이상 나왔을 경우 좋은수를 발견할 수 있다.
-                if dic[temp] >= 3:
-                    ans += dic[temp]
-                    dic[temp] = 0
-                    
-            # 두 수 중 하나만 합한 수와 같을 경우 (0 + 1 = 1)
-            elif temp == li[i] or temp == li[j]:
-                # 2개 이상 나왔을 경우 좋은수를 발견할 수 있다.
-                if dic[temp] >= 2:
-                    ans += dic[temp]
-                    dic[temp] = 0
-                    
-            # 두 수 모두 합한 수와 다를 경우 (1 + 2 = 3)
-            else:
-                # 1개 이상 나왔을 경우 좋은수를 발견할 수 있다.
-                if dic[temp] >= 1:
-                    ans += dic[temp]
-                    dic[temp] = 0
+        # num을 제외한 temp_li를 만든다.
+        temp_li = li[:idx] + li[idx+1:]
+        
+        # 맨 왼쪽 temp_li[0], 맨 오른쪽 temp_li[n-2]
+        s, e = 0, n-2
+        while s < e:
+            sum_val = temp_li[s] + temp_li[e]
+            if sum_val < num:
+                s += 1
+            elif sum_val > num:
+                e -= 1
+            elif sum_val == num: # 좋은 수가 있으면 +1하고 break
+                ans += 1
+                break
     return ans
 
 if __name__ == '__main__':
@@ -62,7 +45,7 @@ if __name__ == '__main__':
     
     # input
     n = int(input())
-    li = list(map(int, input().split()))
+    li = sorted(map(int, input().split()))
     
     # output
     print(solution(n, li))
