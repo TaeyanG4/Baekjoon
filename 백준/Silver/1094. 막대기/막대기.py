255a992ea32d36c6965be56a38ed7d398bf66349
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-25 01:40:55 +0900

    [Silver V] Title: 막대기, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -0,0 +1,33 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = bin(int(input()))
+    
+    # output
+    print(n.count("1"))
