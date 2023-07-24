7c3881fb3cf35c2be0768bb0f12438a44ffc4edd
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-24 23:40:54 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 6084 ms, Memory: 355704 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -0,0 +1,60 @@
+# import lines
+#################################
+import sys
+# import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+# https://ddggblog.tistory.com/211
+
+def solution(start):
+    global memo, visited, tree
+    
+    visited[start] = True
+    if len(tree[start]) == 0:
+        memo[start][1] = 1
+        memo[start][0] = 0
+    else:
+        for node in tree[start]:
+            if not visited[node]:
+                solution(node)
+                
+                # 자신이 얼리어답터가 아닌 경우와 얼리어답터인 경우 중 최소값을 선택
+                memo[start][1] += min(memo[node][0], memo[node][1])
+                
+                # 자신이 얼리어답터가 아닌 경우 자식은 얼리어답터여야 함
+                memo[start][0] += memo[node][1]
+        memo[start][1] += 1 # 자신이 얼리어답터인 경우
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    tree = defaultdict(list)
+    for _ in range(n - 1):
+        a, b = map(int, input().split())
+        tree[a].append(b)
+        tree[b].append(a)
+    
+    memo = [[0, 0] for _ in range(n + 1)]
+    visited = [False for _ in range(n + 1)]
+    
+    # output
+    solution(1)
+    print(min(memo[1][0], memo[1][1]))
