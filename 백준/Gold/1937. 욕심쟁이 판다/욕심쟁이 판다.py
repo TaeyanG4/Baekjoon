0383866be4d97b9e4f1c49b2965644e057034db4
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-26 17:06:04 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 976 ms, Memory: 53520 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,49 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution(y, x):
+    if visited[y][x]: 
+        return visited[y][x]
+    else:
+        visited[y][x] = 1
+
+    for dy, dx in direction:
+        ny = y + dy
+        nx = x + dx
+        if 0 <= ny < n and 0 <= nx < n and forest[y][x] < forest[ny][nx]:
+            visited[y][x] = max(visited[y][x], solution(ny, nx) + 1)
+    
+    return visited[y][x]
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    sys.setrecursionlimit(10**6)
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    forest = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[0] * n for _ in range(n)]
+    
+    # output
+    for i in range(n):
+        for j in range(n):
+            solution(i, j)
+    print(max(map(max, visited)))
