4f86a5be5956fcae9f48b979dff6ce28ce4774ea
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-30 22:35:33 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,22 @@
+from collections import *
+
+def solution(tickets):
+    answer = []
+    
+    dic = defaultdict(list)
+    for departure, arrival in tickets:
+        dic[departure].append(arrival)
+        
+    for key in dic.keys():
+        dic[key].sort(reverse=True)
+    
+    stack = ['ICN']
+    while stack:
+        cur = stack[-1]
+        if cur not in dic or len(dic[cur]) == 0:
+            answer.append(stack.pop())
+        else:
+            stack.append(dic[cur].pop())
+    
+    answer.reverse()
+    return answer
