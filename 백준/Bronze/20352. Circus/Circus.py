2e43b40e67f47c48b58f0272364c8962043d2239
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-07-30 15:17:42 +0900

    [Bronze IV] Title: Circus, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.py
@@ -0,0 +1,34 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    a = int(input())
+    
+    
+    # output
+    print((a/math.pi)**0.5*2*math.pi)
