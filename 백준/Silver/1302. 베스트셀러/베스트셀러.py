a643c6518a0a6a676807b47176c2f6a5d5888418
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-09 04:38:16 +0900

    [Silver IV] Title: 베스트셀러, Time: 68 ms, Memory: 36228 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,39 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    dic = defaultdict(int)
+    for _ in range(n):
+        dic[input().rstrip()] += 1
+    
+    li = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
+    
+    # output
+    print(li[0][0])
