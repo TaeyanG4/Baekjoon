a634d2b6bb5af83b9839fe7760a817b27b562f47
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-09 03:57:09 +0900

    [Silver IV] Title: 접미사 배열, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.py
@@ -0,0 +1,39 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    s = input().rstrip()
+    li = []
+    for i in range(len(s)):
+        li.append(s[i:])
+    li.sort()
+    
+    # output
+    for i in li:
+        print(i)
