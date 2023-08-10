08e6f8e19699eb38d115a696b33b09eef7ec471d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-10 14:41:15 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 72 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,50 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a < b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    dp = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+    
+    # output
+    while True:
+        a, b, c = map(int, input().split())
+        if a == -1 and b == -1 and c == -1:
+            break
+        print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+                
