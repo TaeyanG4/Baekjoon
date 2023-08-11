3702d868a9ab8e8fbb6082ba3debc2a17b8d0680
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-11 13:53:55 +0900

    [Silver II] Title: 연속합, Time: 108 ms, Memory: 41088 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,37 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    li = list(map(int, input().split()))
+    for i in range(1, n):
+        li[i] = max(li[i], li[i-1] + li[i])
+
+    # output
+    print(max(li))
