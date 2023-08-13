d717aa1009f9440c2fba2ac0f332084fad81cfd7
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-13 20:15:00 +0900

    [Platinum V] Title: 히스토그램, Time: 112 ms, Memory: 39840 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -0,0 +1,53 @@
+# import lines
+#################################
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution(n, li):
+    stack = []
+    ans = 0
+    for i in range(n):
+        while stack and li[stack[-1]] > li[i]:
+            height = li[stack.pop()]
+            width = i
+            if stack:
+                width = i - stack[-1] - 1
+            ans = max(ans, height * width)
+        stack.append(i)
+    
+    while stack:
+        height = li[stack.pop()]
+        width = n
+        if stack:
+            width = n - stack[-1] - 1
+        ans = max(ans, height * width)
+    
+    return ans
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    li = [int(input()) for _ in range(n)]
+    
+    # output
+    print(solution(n, li))
