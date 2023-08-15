d1d7b4a1423c41ff5efefd427556dfd5b791be0f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-15 20:59:39 +0900

    [Gold IV] Title: 스도쿠, Time: 712 ms, Memory: 119368 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -18,39 +18,24 @@ import math
 # from bisect import *
 #################################
 
-def row_check(row, num):
-    for i in range(9):
-        if sudoku[row][i] == num:
-            return False
-    return True
-
-def col_check(col, num):
-    for i in range(9):
-        if sudoku[i][col] == num:
-            return False
-    return True
-
-def square_check(row, col, num):
-    nr = (row // 3) * 3
-    nc = (col // 3) * 3
-    for r in range(3):
-        for c in range(3):
-            if sudoku[nr + r][nc + c] == num:
-                return False
-    return True
-
 def dfs(depth):
     if depth == max_depth:
-        for row in sudoku:
-            print(*row)
+        for i in range(9):
+            print(*sudoku[i])
         exit(0)
-        
-    nr, nc = zeros[depth]
-    for num in range(1, 10):
-        if row_check(nr, num) and col_check(nc, num) and square_check(nr, nc, num):
-            sudoku[nr][nc] = num
-            dfs(depth + 1)
-            sudoku[nr][nc] = 0
+    
+    nc, nr = zeros[depth]
+    nsq = (nc//3)*3 + nr//3
+    for i in range(1, 10):
+        if not row[nc][i] and not col[nr][i] and not square[nsq][i]:
+            row[nc][i] = True
+            col[nr][i] = True
+            square[nsq][i] = True
+            sudoku[nc][nr] = i
+            dfs(depth+1)
+            row[nc][i] = False
+            col[nr][i] = False
+            square[nsq][i] = False
 
 if __name__ == '__main__':
     input = sys.stdin.readline
@@ -60,9 +45,23 @@ if __name__ == '__main__':
     # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
     # input
-    sudoku = [list(map(int, input().split())) for _ in range(9)]
-    zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
+    sudoku = [[False for _ in range(9)] for _ in range(9)]
+    row = [[False for _ in range(10)] for _ in range(9)]
+    col = [[False for _ in range(10)] for _ in range(9)]
+    square = [[False for _ in range(10)] for _ in range(9)]
+    
+    zeros = []
+    for i in range(9):
+        li = [*map(int, input().split())]
+        for j in range(9):
+            sudoku[i][j] = li[j]
+            if li[j]:
+                row[i][li[j]] = True
+                col[j][li[j]] = True
+                square[(i//3)*3 + j//3][li[j]] = True
+            else:
+                zeros.append((i, j))
     max_depth = len(zeros)
-
+    
     # output
     dfs(0)
