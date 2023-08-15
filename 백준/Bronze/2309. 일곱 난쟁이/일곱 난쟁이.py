cf92d5a25042ab0e60d89998625fb477318f5ca5
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-15 14:36:57 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,39 @@
+## taeyang's template (1.0.0)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+from itertools import *
+# from statistics import *
+# from datetime import datetime
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    li = [int(input()) for _ in range(9)]
+    
+    # output
+    for i in combinations(li, 7):
+        if sum(i) == 100:
+            for j in sorted(i):
+                print(j)
+            break
