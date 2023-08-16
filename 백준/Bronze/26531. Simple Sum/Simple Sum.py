fc6e2e8b1078559d5244e6b787f45d58f12d1a3d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-17 06:02:32 +0900

    [Bronze IV] Title: Simple Sum, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.py
@@ -0,0 +1,35 @@
+## taeyang's template (1.0.1)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    s, ans = input().rstrip().split('=')
+    
+    # output
+    print('YES' if eval(s) == int(ans) else 'NO')
