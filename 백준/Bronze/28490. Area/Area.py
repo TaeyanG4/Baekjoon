78bb11c34dfed05bcf45de157029138df19946a8
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-19 16:17:21 +0900

    [Bronze IV] Title: Area, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/28490. Area/Area.py
@@ -0,0 +1,39 @@
+## taeyang's template (1.0.1)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n = int(input())
+    ans = -INF
+    for i in range(n):
+        a, b = map(int, input().split())
+        ans = max(ans, a*b)
+    
+    # output
+    print(ans)
