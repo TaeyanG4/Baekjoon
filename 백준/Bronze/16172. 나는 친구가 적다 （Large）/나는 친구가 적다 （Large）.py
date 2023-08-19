e86e8e8cbd0727bd200108fc3277ccb3186274c3
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-20 00:33:37 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 68 ms, Memory: 33736 KB -BaekjoonHub

---
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.py
@@ -0,0 +1,41 @@
+## taeyang's template (1.0.1)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+# from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+#################################
+
+def solution(s, keyword):
+    new_s = ''
+    for c in s:
+        if c.isalpha():
+            new_s += c
+    
+    return 1 if keyword in new_s else 0
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    s = input().rstrip()
+    keyword = input().rstrip()
+    
+    # output
+    print(solution(s, keyword))
