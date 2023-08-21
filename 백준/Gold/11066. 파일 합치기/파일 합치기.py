de1585326d8d85b3d38fa7b36cf7f098c7d81b07
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-22 00:38:15 +0900

    [Gold III] Title: 파일 합치기, Time: 1748 ms, Memory: 127632 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,9 +1,10 @@
 ## taeyang's template (1.0.1)
 # https://post.naver.com/viewer/postView.nhn?volumeNo=28894924&memberNo=33264526&vType=VERTICAL
+# https://suri78.tistory.com/15
 #################################
 ## my import lines
 import sys
-import math
+# import math
 # import copy
 # import ast
 # import re
@@ -19,20 +20,41 @@ import math
 # from bisect import *
 #################################
 
+# def solution(k, li):
+    
+#     dp = [[0] * (k+1) for _ in range(k+1)]
+#     prefix_sum = [0] * (k+1)
+#     for i in range(k):
+#         prefix_sum[i] = prefix_sum[i-1] + li[i]
+    
+#     for g in range(1, k):
+#         for s in range(k-g):
+#             e = s + g
+#             dp[s][e] = INF
+#             for i in range(s, e):
+#                 dp[s][e] = min(dp[s][e], dp[s][i] + dp[i+1][e] + prefix_sum[e] - prefix_sum[s-1])
+#     # print(prefix_sum)
+#     # pprint.pprint(dp)
+#     return dp[0][k-1]
+
 def solution(k, li):
     
-    dp = [[0] * (k+1) for _ in range(k+1)]
-    prefix_sum = [0] * (k+1)
-    for i in range(k):
-        prefix_sum[i] = prefix_sum[i-1] + li[i]
-    
-    for g in range(1, k):
-        for s in range(k-g+1):
+    # dp + prefix_sum
+    dp = [[0] * (k) for _ in range(k)]
+    for i in range(k-1):
+        dp[i][i+1] = li[i] + li[i+1]
+        for j in range(i+2, k):
+            dp[i][j] = dp[i][j-1] + li[j]
+            
+    for g in range(2, k):
+        for s in range(k-g):
             e = s + g
-            dp[s][e] = INF
+            min_vals = []
             for i in range(s, e):
-                dp[s][e] = min(dp[s][e], dp[s][i] + dp[i+1][e] + prefix_sum[e] - prefix_sum[s-1])
-                
+                min_vals.append(dp[s][i] + dp[i+1][e])
+            dp[s][e] += min(min_vals)
+
+    # pprint.pprint(dp)
     return dp[0][k-1]
 
 if __name__ == '__main__':
