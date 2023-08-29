0040ecc310b0b8606a5355d902c5da96ebc95df9
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-08-29 13:46:53 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 764 ms, Memory: 83736 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,51 @@
+## taeyang's template (1.0.1)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import time
+# import pprint
+from collections import *
+# from heapq import *
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+#################################
+
+def dfs(start):
+    global ans, cnt
+    visited[start] = True
+    cnt += 1
+    ans[start] = cnt
+    for node in sorted(graph[start]):
+        if not visited[node]:
+            dfs(node)
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = sys.maxsize
+    # MOD = 10**9 + 7
+    sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n, m, r = map(int, input().split())
+    graph = defaultdict(list)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    visited = [False] * (n + 1)
+    ans, cnt = [0] * (n+1), 0
+
+    # output
+    dfs(r)
+    ans.pop(0)
+    for v in ans:
+        print(v)
