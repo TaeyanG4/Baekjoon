17d67c0b8245422c1d827632705a97e5f2f28d21
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-02 21:30:32 +0900

    [Gold I] Title: 냅색문제, Time: 160 ms, Memory: 117356 KB -BaekjoonHub

---
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -0,0 +1,66 @@
+## taeyang's template (1.0.4)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+def solution():
+    left_li =  li[:n//2]
+    right_li = li[n//2:]
+    left_sum, right_sum = [], []
+    
+    for i in range(len(left_li)+1):
+        for comb in combinations(left_li, i):
+            left_sum.append(sum(comb))
+            
+    for i in range(len(right_li)+1):
+        for comb in combinations(right_li, i):
+            right_sum.append(sum(comb))
+    
+    left_sum.sort()
+    ans = 0
+    for r in right_sum:
+        
+        if r > c:
+            continue
+        
+        s, e = 0, len(left_sum) - 1
+        while s <= e:
+            m = (s + e) // 2
+            if left_sum[m] + r > c:
+                e = m - 1
+            else:
+                s = m + 1
+        ans += e + 1
+        
+    return ans
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n, c = map(int, input().split())
+    li = [*map(int, input().split())]
+
+    # output
+    print(solution())
