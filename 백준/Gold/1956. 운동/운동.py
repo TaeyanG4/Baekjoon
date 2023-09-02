cf74d18a4440519a24bfff826a78c049f6e22f85
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-02 20:30:32 +0900

    [Gold IV] Title: 운동, Time: 976 ms, Memory: 117636 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,23 +1,51 @@
+## taeyang's template (1.0.3)
+#################################
+## my import lines
 import sys
+# import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
 
-def problem1956():
-    n, m = map(int, sys.stdin.readline().split())
+# def solution(n, m, dist):
+#     pass
+
+if __name__ == '__main__':
+    # input = sys.stdin.readline
     INF = float('inf')
-    distance = [[INF]*n for _ in range(n)]
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n, m = map(int, sys.stdin.readline().split())
+    dist = [[INF] * n for _ in range(n)]
     for _ in range(m):
-        u, v, w = map(int, sys.stdin.readline().split())
-        distance[u-1][v-1] = w
-
-    for waypoint in range(n):
+        a, b, d = map(int, sys.stdin.readline().split())
+        dist[a-1][b-1] = d
+    
+    # floyd-warshall
+    for k in range(n):
         for i in range(n):
             for j in range(n):
-                if distance[i][j] > distance[i][waypoint] + distance[waypoint][j]:
-                    distance[i][j] = distance[i][waypoint] + distance[waypoint][j]
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
 
-    result = INF
+    # output
+    ans = INF
     for i in range(n):
-        result = min(result, distance[i][i])
-    print(result if result != INF else -1)
-
-    
-problem1956()
+        ans = min(ans, dist[i][i])
+    print(-1 if ans == INF else ans)
