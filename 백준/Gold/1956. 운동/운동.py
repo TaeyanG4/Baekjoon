d7e2925ca43fa99a4f08f8a415e4fa4bbe15861f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-02 20:21:28 +0900

    [Gold IV] Title: 운동, Time: 5688 ms, Memory: 36896 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,52 +1,23 @@
-## taeyang's template (1.0.3)
-#################################
-## my import lines
 import sys
-# import math
-# import copy
-# import ast
-# import re
-# import time
-# import json
-# import pprint
-# from collections import *
-# from heapq import *
-# from queue import PriorityQueue
-# from itertools import *
-# from statistics import *
-# from datetime import *
-# from bisect import *
-# from fractions import Fraction
-# from decimal import *
-#################################
 
-def solution(n, m, dist):
-    
-    ans = INF
-    for i in range(n):
-        ans = min(ans, dist[i][i])
-    return -1 if ans == INF else ans
-
-if __name__ == '__main__':
-    input = sys.stdin.readline
+def problem1956():
+    n, m = map(int, sys.stdin.readline().split())
     INF = float('inf')
-    # MOD = 10**9 + 7
-    # sys.setrecursionlimit(10**6)
-    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
-    # input
-    n, m = map(int, input().split())
-    dist = [[INF] * n for _ in range(n)]
+    distance = [[INF]*n for _ in range(n)]
     for _ in range(m):
-        a, b, d = map(int, input().split())
-        dist[a-1][b-1] = d
-    
-    # floyd-warshall
-    for k in range(n):
+        u, v, w = map(int, sys.stdin.readline().split())
+        distance[u-1][v-1] = w
+
+    for waypoint in range(n):
         for i in range(n):
             for j in range(n):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
+                if distance[i][j] > distance[i][waypoint] + distance[waypoint][j]:
+                    distance[i][j] = distance[i][waypoint] + distance[waypoint][j]
 
-    # output
-    print(solution(n, m, dist))
+    result = INF
+    for i in range(n):
+        result = min(result, distance[i][i])
+    print(result if result != INF else -1)
+
+    
+problem1956()
