8dee6362dd32f2d2c9ce20b53609e47ec8aacd2a
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-13 08:22:19 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.py
@@ -0,0 +1,40 @@
+## taeyang's template (1.0.4)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    a1, a2, b1, b2 = map(int, input().split())
+
+    # output
+    if a1 - b1 >= 2 and a2 - b2 >= 2:
+        print(1)
+    else:
+        print(0)
