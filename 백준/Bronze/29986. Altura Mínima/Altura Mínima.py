460fdd042127d507304d2eedabbde320b0d3d2a2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-15 06:05:37 +0900

    [Bronze IV] Title: Altura Mínima, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/29986. Altura Mínima/Altura Mínima.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.4)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    n, m = map(int, input().split())
+    li = [*map(int, input().split())]
+    # n = int(input())
+    
+    # output
+    ans = 0
+    for i in li:
+        if i <= m:
+            ans += 1
+    print(ans)
