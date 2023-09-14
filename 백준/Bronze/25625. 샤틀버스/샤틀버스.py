456c1fae693aa212a729111dd374899aac0ba647
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-15 05:58:27 +0900

    [Bronze IV] Title: 샤틀버스, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.py
@@ -0,0 +1,41 @@
+## taeyang's template (1.0.4)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    # input
+    # n, m = map(int, input().split())
+    # li = [*map(int, input().split())]
+    # n = int(input())
+    
+    x, y = map(int, input().split())
+    
+    # output
+    print(x + y if x > y else y - x)
