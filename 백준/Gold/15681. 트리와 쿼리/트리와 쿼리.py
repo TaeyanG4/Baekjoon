128449e7eea459ad1ccddffe648a7853e9cc8fc9
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-20 00:01:10 +0900

    [Gold V] Title: 트리와 쿼리, Time: 504 ms, Memory: 73908 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,58 +1,50 @@
 ## taeyang's template (1.0.7)
+# https://www.acmicpc.net/problem/2213 # 트리의 독립집합
 # https://jainn.tistory.com/74
 #################################
 ## my import lines
 import sys
 import math
-# import copy
-# import ast
-# import re
-# import time
-# import json
-# import pprint
 from collections import *
-# from heapq import *
-# from queue import PriorityQueue
-# from itertools import *
-# from statistics import *
-# from datetime import *
-# from bisect import *
-# from fractions import Fraction
-# from decimal import *
 #################################
 
-
-def solution(x):
+def get_subtree_cnt(x):
     cnt[x] = 1
     for i in graph[x]:
         if not cnt[i]:
-            solution(i) # 서브트리의 노드 개수를 구하기위한 dfs
-            cnt[x] += cnt[i]
-
+            cnt[x] += get_subtree_cnt(i) # 서브트리의 노드 개수를 구하기위한 dfs
+    return cnt[x]
 
 if __name__ == "__main__":
     input = sys.stdin.readline
     S = lambda: map(int, input().split())
-    # INF = float('inf')
-    # MOD = 10**9 + 7
-    sys.setrecursionlimit(10**6)
-    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    # input
-    # n, m = map(int, input().split())
-    # lst = [*map(int, input().split())]
-    # n = int(input())
+    sys.setrecursionlimit(10**9)
     
-    n, r, q = map(int, input().split())
-    graph = defaultdict(list)
+    n, r, q = map(int, input().split()) # n: 정점의 개수, r: 루트 노드, q: 쿼리의 개수
     cnt = [0] * (n+1)
+    graph = defaultdict(list)
     for _ in range(n-1):
         u, v = map(int, input().split())
         graph[u].append(v)
         graph[v].append(u)
     
-    solution(r)
+    get_subtree_cnt(r) # r을 루트로 하는 트리의 서브트리의 노드 개수를 구함
     
     for _ in range(q):
+        
         # output
         print(cnt[int(input())])
+
+## input 1
+# 9 5 3
+# 1 3
+# 4 3
+# 5 4
+# 5 6
+# 6 7
+# 2 3
+# 9 6
+# 6 8
+# 5
+# 4
+# 8
