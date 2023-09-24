b21619bd40facbcda71b7ba0153c296b2940f44d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-25 00:21:18 +0900

    [Platinum IV] Title: 광고, Time: 480 ms, Memory: 73916 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.py
@@ -0,0 +1,58 @@
+## taeyang's template (1.0.7)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+def compute_lps(pat: str) -> int:
+    pt = 1 
+    pp = 0  
+    skip = [0] * (len(pat) + 1)  
+
+    skip[pt] = 0
+    while pt != len(pat):
+        if pat[pt] == pat[pp]:
+            pt += 1
+            pp += 1
+            skip[pt] = pp
+        elif pp == 0:
+            pt += 1
+            skip[pt] = pp
+        else:
+            pp = skip[pp]
+    
+    return skip
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    # n, m = map(int, input().split())
+    # lst = [*map(int, input().split())]
+    n = int(input())
+    s = input().rstrip()
+    lps = compute_lps(s)
+
+    # output
+    print(len(s) - lps[-1])
