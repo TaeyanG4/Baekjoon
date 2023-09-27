c0b47a0d2ee031b8fe32e92a570ba706424eba40
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-27 14:24:14 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.7)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    if (a<=1 and b<=2) or (a<=2 and b<=1):
+        return 'Yes'
+    return 'No'
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    
+    # input
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    if a+b > c:
+        print(0)
+    else:
+        print(1)
