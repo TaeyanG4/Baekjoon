e26bc4340a60c0abb7698325a1ff23415dba30c2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-29 14:42:37 +0900

    [Bronze IV] Title: Affischutskicket, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/24183. Affischutskicket/Affischutskicket.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.7)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    a, b ,c = S()
+    c4 = (0.229*0.324)*a*2
+    a3 = (0.297*0.420)*b*2
+    a4 = (0.210*0.297)*c
+    
+
+    # output
+    print(f'{c4+a3+a4:.3f}')
