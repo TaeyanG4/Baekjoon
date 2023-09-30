6f0825329ceb955fd95b3709a22e9f87223fde90
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-09-30 10:46:28 +0900

    [Bronze IV] Title: 短針 (Hour Hand), Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/24083. 短針 （Hour Hand）/短針 （Hour Hand）.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.7)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    a = int(input())
+    b = int(input())
+    # tmp = ((a + b) // 12)
+    ans = ((a + b) % 12)
+    
+    # output
+    print(12 if ans == 0 else ans)
