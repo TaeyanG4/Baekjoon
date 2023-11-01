71771162b6f21bbb162db8a88e991a8dbb48a79c
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-11-02 08:26:21 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 140 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    n = int(input())
+    memo = [*map(int, input().split())]
+    
+    # dp
+    for i in range(1, n):
+        for j in range((i//2)+1):
+            memo[i] = min(memo[i], memo[j] + memo[i-j-1])
+    
+    # output
+    print(memo[-1])
