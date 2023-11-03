b1742dba5698ff3e98a061b6081e1234fd64cbc5
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-11-03 13:27:48 +0900

    [Bronze IV] Title: Граничные клетки, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/27213. Граничные клетки/Граничные клетки.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    n = int(input())
+    m = int(input())
+    
+    # output
+    if n > 1 and m > 1:
+        print(((n+m)*2)-4)
+    else:
+        print(n*m)
