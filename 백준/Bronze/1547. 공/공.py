a99654db00f79125f5f7b1c9f09e5e7e2599160d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-11-12 06:53:11 +0900

    [Bronze III] Title: 공, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def swap():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    n = int(input())
+    lst = [1, 2, 3]
+    for _ in range(n):
+        x, y = S()
+        lst[x-1], lst[y-1] = lst[y-1], lst[x-1]
+
+    # output
+    print(lst.index(1)+1)
