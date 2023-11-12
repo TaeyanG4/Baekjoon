2c5a200fd78844107f33bd5d05e99b5d8ebebe35
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-11-13 06:05:35 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 128 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,49 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    MOD = 10**9 + 9
+    # sys.setrecursionlimit(10**6)
+    # direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # input
+    t = int(input())
+    
+    # dp
+    memo = [0] * (10 ** 6 + 1)
+    memo[0], memo[1], memo[2] = 1, 2, 4
+    for i in range(3, 10 ** 6 + 1):
+        memo[i] = (memo[i - 1] + memo[i - 2] + memo[i - 3]) % MOD
+    
+    # output
+    for _ in range(t):
+        n = int(input())
+        print(memo[n - 1])
