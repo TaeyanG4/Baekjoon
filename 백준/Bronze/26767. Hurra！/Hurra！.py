3718233dc06315b2d5d6ed8bb9a417e05e2e002f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-12-07 06:05:51 +0900

    [Bronze IV] Title: Hurra!, Time: 688 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/26767. Hurra！/Hurra！.py
@@ -0,0 +1,49 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    n = int(input())
+    
+    # output
+    for i in range(1, n+1):
+        if i % 7 == 0 and i % 11 == 0:
+            print('Wiwat!')
+        elif i % 7 == 0:
+            print('Hurra!')
+        elif i % 11 == 0:
+            print('Super!')
+        else:
+            print(i)
