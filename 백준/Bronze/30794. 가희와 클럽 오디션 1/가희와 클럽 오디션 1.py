c70468752e8de2af31cf17939eeb9e8d701b21b4
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-12-24 17:11:24 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.py
@@ -0,0 +1,50 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    n, s = map(str, input().split())
+    
+    # output
+    if s == 'miss':
+        print(0)
+    elif s == 'bad':
+        print(int(n) * 200)
+    elif s == 'cool':
+        print(int(n) * 400)
+    elif s == 'great':
+        print(int(n) * 600)
+    elif s == 'perfect':
+        print(int(n) * 1000)
