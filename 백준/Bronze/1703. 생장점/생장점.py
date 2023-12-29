e6963cfc58f8fd4d9c3158f803f8287979766b12
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2023-12-30 06:47:53 +0900

    [Bronze III] Title: 생장점, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/1703. 생장점/생장점.py
@@ -0,0 +1,50 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    while True:
+        
+        #input
+        a, *lst = [*map(int, input().split())]
+        if a == 0:
+            break
+        
+        ans = 1
+        for i in range(0, a*2, 2):
+            ans *= lst[i]
+            ans -= lst[i+1]
+            
+        # output
+        print(ans)
