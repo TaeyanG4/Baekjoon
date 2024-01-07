344612c7ab3d94e7c7e44037f0de628bffb08609
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-01-07 17:25:50 +0900

    [Bronze III] Title: 삼각수의 합, Time: 64 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def t(k):
+    return k * (k + 1) // 2
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    for _ in range(int(input())):
+        # W(n) = Sum[k=1..n; k*T(k+1)]
+        ans = 0
+        for k in range(int(input()) + 1):
+            ans += k * t(k + 1)
+        print(ans)
