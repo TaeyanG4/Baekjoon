65c93b1baa06c97af51aa565daea87b39f7dc722
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-01-25 15:13:18 +0900

    [Bronze III] Title: 등장하지 않는 문자의 합, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/3059. 등장하지 않는 문자의 합/등장하지 않는 문자의 합.py
@@ -0,0 +1,49 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    t = int(input())
+    aski = 0 
+    for i in range(65, 91):
+        aski += i
+    
+    # output    
+    for _ in range(t):
+        sm = 0
+        s = set(list(input().rstrip()))
+        for i in s:
+            sm += ord(i)
+        print(aski - sm)
