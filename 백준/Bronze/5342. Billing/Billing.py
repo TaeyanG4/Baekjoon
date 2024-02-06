59205134b4841f50fde038d918fa9b7fdc5672a5
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-02-07 08:39:49 +0900

    [Bronze III] Title: Billing, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/5342. Billing/Billing.py
+++ 백준/Bronze/5342. Billing/Billing.py
@@ -34,38 +34,24 @@ if __name__ == "__main__":
     # sys.setrecursionlimit(10**6)
     # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-    # # input
-    # dic = {
-    #     "Paper": 57.99,
-    #     "Printer": 125.50,
-    #     "Planners": 31.25,
-    #     "Binders": 22.50,
-    #     "Calendar": 10.95,
-    #     "Notebooks": 11.20,
-    #     "Ink": 66.95,
-    # }
+    # input
+    dic = {
+        "Paper": 57.99,
+        "Printer": 120.50,
+        "Planners": 31.25,
+        "Binders": 22.50,
+        "Calendar": 10.95,
+        "Notebooks": 11.20,
+        "Ink": 66.95,
+    }
     
-    # ans = 0
-    # while True:
-    #     s = input().strip()
-    #     if s == "EOI":
-    #         break
-    #     ans += dic[s]
-    
-    # # output
-    # print(f'${ans:.2f}')
-    costs = {
-        "Paper" : 57.99, 
-        "Printer" : 120.50, 
-        "Planners" : 31.25, 
-        "Binders" : 22.50, 
-        "Calendar" : 10.95, 
-        "Notebooks" : 11.20, 
-        "Ink" : 66.95}
-    total = 0
-    while True :
+    ans = 0
+    while True:
         s = input().strip()
-        if s == "EOI" : break
-        total += costs[s]
-        
-    print("${:.2f}".format(total))
+        if s == "EOI":
+            break
+        ans += dic[s]
+    
+    # output
+    print(f'${ans:.2f}')
+    
