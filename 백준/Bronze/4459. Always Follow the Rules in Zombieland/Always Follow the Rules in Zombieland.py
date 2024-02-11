5b336fa6cdecd9c35ff361e2e0ec77ace536e5a4
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-02-11 15:23:16 +0900

    [Bronze III] Title: Always Follow the Rules in Zombieland, Time: 64 ms, Memory: 36152 KB -BaekjoonHub

---
+++ 백준/Bronze/4459. Always Follow the Rules in Zombieland/Always Follow the Rules in Zombieland.py
@@ -0,0 +1,48 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    dic = defaultdict(str)
+    for i in range(int(input())):
+        dic[i+1] = input().strip()
+    
+    # output
+    for _ in range(int(input())):
+        n = int(input())
+        if dic[n] == "":
+            print(f"Rule {n}: No such rule")
+        else:
+            print(f"Rule {n}: {dic[n]}")
