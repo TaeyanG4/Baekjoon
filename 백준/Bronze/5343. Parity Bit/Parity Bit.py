369f584b551bd93dfd14ca87f3da729115dbf90e
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-03-17 06:29:07 +0900

    [Bronze III] Title: Parity Bit, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5343. Parity Bit/Parity Bit.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    for _ in range(int(input())):
+        s = input().strip()
+        ans = 0
+        for i in range(0, len(s), 8):
+            cnt = s[i:i+7].count('1')
+            if cnt % 2 != int(s[i+7]):
+                ans += 1
+                
+        # output
+        print(ans)
