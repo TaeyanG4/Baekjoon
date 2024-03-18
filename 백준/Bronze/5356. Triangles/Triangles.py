49403954295666fc5add767bdd8f9ebd17fc3ac5
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-03-19 06:09:52 +0900

    [Bronze III] Title: Triangles, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5356. Triangles/Triangles.py
@@ -0,0 +1,49 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for _ in range(int(input())):
+        n, alpha = map(str, input().split())
+        p = abc.find(alpha)
+        for i in range(int(n)):
+            
+            if p+i >= 26:
+                p -= 26
+        
+            # output
+            print(abc[p+i]*(i+1))
+        print()
