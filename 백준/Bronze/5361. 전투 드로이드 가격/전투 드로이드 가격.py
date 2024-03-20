de2571cde2f234e3dd868f62446059ebdc1202d2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-03-20 11:06:57 +0900

    [Bronze III] Title: 전투 드로이드 가격, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.py
@@ -0,0 +1,39 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    for _ in range(int(input())):
+        a, b, c, d ,e = S()
+        print(f'$%.2f' % (a*350.34 + b*230.90 + c*190.55 + d*125.30 + e*180.90))
