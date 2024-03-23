209971bbb4c23a953eb5d878acc2199473f1a832
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-03-24 07:05:45 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 56 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.py
@@ -0,0 +1,51 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    n = int(input())
+    m = int(input())
+    mx = m
+    for _ in range(n):
+        a, b = S()
+        m += (a-b)
+        if m < 0:
+            break
+        mx = max(mx, m)
+    
+    if m < 0:
+        print(0)
+    else:
+        print(mx)
