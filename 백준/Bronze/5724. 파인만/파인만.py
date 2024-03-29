1b25065b7140917a69064e8339aeb3f8230db208
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-03-29 11:09:05 +0900

    [Bronze III] Title: 파인만, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/5724. 파인만/파인만.py
+++ 백준/Bronze/5724. 파인만/파인만.py
@@ -38,7 +38,4 @@ if __name__ == "__main__":
         n = int(input())
         if n == 0:
             break
-        ans = 0
-        for i in range(1, n+1):
-            ans += i**2
-        print(ans)
+        print(n*(n+1)*(2*n+1)//6)
