230d53efa2d6771af6604b42b551446647413bde
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-04-23 12:13:39 +0900

    [Bronze III] Title: Sumac Sequences, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/6780. Sumac Sequences/Sumac Sequences.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    n, m = int(input()), int(input())
+    cnt = 2
+    while True:
+        tmp = n - m
+        if tmp < 0:
+            print(cnt)
+            break
+        n, m = m, tmp
+        cnt += 1
