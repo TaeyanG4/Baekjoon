93e859237c67de8db54d50bedae2527ed1eb482f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-06-15 06:32:43 +0900

    [Bronze III] Title: 개표, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    int(input())
+    a, b = 0, 0
+    for c in input().strip():
+        if c == "A":
+            a += 1
+        else:
+            b += 1
+    print("A" if a > b else "B" if a < b else "Tie")
