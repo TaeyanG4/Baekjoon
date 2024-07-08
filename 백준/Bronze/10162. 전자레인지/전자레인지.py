0d7fe368a3ac47a0db05e07759e83394f28ebaf3
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-07-08 13:52:38 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,53 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # input
+    # n = int(input())
+    # n, m = map(int, input().split())
+    # lst = [*map(int, input().split())]
+    
+    n = int(input())
+    a, b, c = 0, 0, 0
+    a = n // 300
+    n %= 300
+    b = n // 60
+    n %= 60
+    c = n // 10
+    n %= 10
+    if n:
+        print(-1)
+    else:
+        print(a, b, c)
