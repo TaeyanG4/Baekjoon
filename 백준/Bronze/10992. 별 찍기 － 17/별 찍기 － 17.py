cd0d0507e90de532c4d25893651f655a3f1e7f1f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-07-24 13:25:44 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# from functools import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    for i in range(n-1):
+        print(' '*(n-1-i), end='')
+        print('*', end='')
+        print(' '*(2*i-1), end='')
+        if i != 0:
+            print('*')
+        else:
+            print()
+    print('*'*(2*n-1))
