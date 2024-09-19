6dbc1ae4e01b7cae8ec61c773d598a63c81620e0
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-09-20 07:31:01 +0900

    [Bronze III] Title: Dalia, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/10695. Dalia/Dalia.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    direction = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+    for i in range(int(input())):
+        n, r1, c1, r2, c2 = S()
+        for x, y in direction:
+            if r1 + x == r2 and c1 + y == c2:
+                print(f"Case {i+1}: YES")
+                break
+        else:
+            print(f"Case {i+1}: NO")
