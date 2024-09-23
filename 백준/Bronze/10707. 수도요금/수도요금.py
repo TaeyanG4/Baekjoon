368623dd5f51e4435c002d49ae99d608b79ab96d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-09-23 09:10:43 +0900

    [Bronze III] Title: 수도요금, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    p = int(input())
+    x_pay = a * p
+    if c > p:
+        y_pay = b
+    else:
+        y_pay = b + ((p - c) * d)
+    print(min(x_pay, y_pay))
