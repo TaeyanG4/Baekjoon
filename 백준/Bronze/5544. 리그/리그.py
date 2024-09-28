9d1c523e036d67e4746e4232861e9b46ccf8bf5a
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-09-29 07:40:37 +0900

    [Bronze I] Title: 리그, Time: 52 ms, Memory: 36176 KB -BaekjoonHub

---
+++ 백준/Bronze/5544. 리그/리그.py
@@ -0,0 +1,51 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    dic = defaultdict(int)
+    
+    for i in range(n*(n-1)//2):
+        a, b, c, d = S()
+        if c > d:
+            dic[a] += 3
+        elif c < d:
+            dic[b] += 3
+        else:
+            dic[a] += 1
+            dic[b] += 1
+            
+    lst = sorted(dic.values(), reverse=True)
+    for i in range(1, n+1):
+        print(lst.index(dic[i])+1)
