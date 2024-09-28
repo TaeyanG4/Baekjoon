6b77f0e9e7b042bbab6e221f05ee31a41309e121
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-09-29 08:47:57 +0900

    [Bronze III] Title: Golden, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/10187. Golden/Golden.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(float, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # golden ratio
+    phi = (1 + 5 ** 0.5) / 2
+    for _ in range(int(input())):
+        a, b = S()
+        if (phi/100) * 99 <= a/b <= (phi/100) * 101:
+            print('golden')
+        else:
+            print('not')
