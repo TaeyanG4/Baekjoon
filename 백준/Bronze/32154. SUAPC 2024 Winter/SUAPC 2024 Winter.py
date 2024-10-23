d54ecd1766e45861b3f75f47cd4e20070baf1469
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-10-23 10:23:06 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,65 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    if n == 1:
+        print(11)
+        print("A B C D E F G H J L M")
+    elif n == 2:
+        print(9)
+        print("A C E F G H I L M")
+    elif n == 3:
+        print(9)
+        print("A C E F G H I L M")
+    elif n == 4:
+        print(9)
+        print("A B C E F G H L M")
+    elif n == 5:
+        print(8)
+        print("A C E F G H L M")
+    elif n == 6:
+        print(8)
+        print("A C E F G H L M")
+    elif n == 7:
+        print(8)
+        print("A C E F G H L M")
+    elif n == 8:
+        print(8)
+        print("A C E F G H L M")
+    elif n == 9:
+        print(8)
+        print("A C E F G H L M")
+    elif n == 10:
+        print(8)
+        print("A B C F G H L M")  
