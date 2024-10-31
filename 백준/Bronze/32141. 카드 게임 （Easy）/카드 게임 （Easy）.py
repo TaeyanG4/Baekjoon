2a0bb2e274e3204b5ee3c982d98dbb095606776a
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-10-31 18:40:24 +0900

    [Bronze II] Title: 카드 게임 (Easy), Time: 320 ms, Memory: 145388 KB -BaekjoonHub

---
+++ 백준/Bronze/32141. 카드 게임 （Easy）/카드 게임 （Easy）.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n, h = S()
+    lst = [*map(int, input().split())]
+    ans = 1
+    for i in lst:
+        if h > i:
+            ans += 1
+            h -= i
+        else:
+            print(ans)
+            break
+    else:
+        print(-1)
