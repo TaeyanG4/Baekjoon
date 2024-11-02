89018deaa33c48d986efcfeab763381cf7e0df57
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-11-03 08:59:14 +0900

    [Bronze III] Title: ZAMKA, Time: 48 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/11874. ZAMKA/ZAMKA.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    l, d, x = int(input()), int(input()), int(input())
+    mi, mx = INF, -INF
+    for i in range(l, d+1):
+          if x == sum(map(int, str(i))):
+              mi = min(mi, i)
+              mx = max(mx, i)
+    print(mi)
+    print(mx)
