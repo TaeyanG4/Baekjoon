2839d28511fbe17e1690dd1b95831c6e8289cce9
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-11-14 18:25:07 +0900

    [Bronze III] Title: 출제비 재분배, Time: 756 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/26145. 출제비 재분배/출제비 재분배.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n, m = S()
+    salary = [*S()]
+    ans = [0] * (n+m)
+    for i in range(n):
+        for j, pay in enumerate(S()):
+            ans[j] += pay
+            salary[i] -= pay
+    
+    for i in range(n):
+        ans[i] += salary[i]
+    print(*ans)
