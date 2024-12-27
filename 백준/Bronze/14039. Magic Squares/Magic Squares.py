29e50ddc46db3e483cf7aa9366f04d87754cef25
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-12-27 17:16:49 +0900

    [Bronze III] Title: Magic Squares, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/14039. Magic Squares/Magic Squares.py
@@ -0,0 +1,37 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    board = [[*S()] for _ in range(4)]
+    print("magic" if (all(sum(row) == sum(board[0]) for row in board) and all(sum(col) == sum(board[0]) for col in zip(*board))) else "not magic")
