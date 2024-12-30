c3ffaf7a368a5312393e463abaa6e248f6fb0736
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2024-12-31 07:46:45 +0900

    [Bronze III] Title: Bridž, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/14209. Bridž/Bridž.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    ans = 0
+    for _ in range(int(input())):
+        for c in input().strip():
+            if c == 'A':
+                ans += 4
+            elif c == 'K':
+                ans += 3
+            elif c == 'Q':
+                ans += 2
+            elif c == 'J':
+                ans += 1
+    print(ans)
