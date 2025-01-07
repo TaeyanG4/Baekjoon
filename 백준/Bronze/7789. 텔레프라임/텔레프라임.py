d9534a2a1aeae6d8913792ff414d37d7c468fc90
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-01-07 09:45:36 +0900

    [Bronze II] Title: 텔레프라임, Time: 56 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Bronze/7789. 텔레프라임/텔레프라임.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def isprime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    v1, v2 = S()
+    v2 = int(str(v2)+str(v1))
+    print("Yes" if isprime(v1) and isprime(v2) else "No")
