577c9033133f7c3b0e15f54889111340cc5ca1d4
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-01-30 08:04:24 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 48 ms, Memory: 35328 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+    
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    board = [['@']*n*5 for _ in range(n*5)]
+    for i in range(n, n*5):
+        for j in range(n, n*2):
+            board[i][j] = ' '
+    for i in range(n*4):
+        for j in range(n*3, n*4):
+            board[i][j] = ' '
+    
+    for line in board:
+        print(''.join(line))
+    
