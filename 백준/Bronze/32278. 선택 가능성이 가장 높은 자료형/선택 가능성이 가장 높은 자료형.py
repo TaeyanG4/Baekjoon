8e737d96d1b9b9d0d9cd2833afc40a452b00452a
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-03-06 07:25:56 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    # INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    if -32768 <= n <= 32767:
+        print("short")
+    elif -2147483648 <= n <= 2147483647:
+        print("int")
+    elif -9223372036854775808 <= n <= 9223372036854775807:
+        print("long long")
