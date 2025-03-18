348d7763cbb054876cb2cb450a088346b8aed85d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-03-18 09:09:56 +0900

    [Bronze IV] Title: Innohorse, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/21875. Innohorse/Innohorse.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    s1, s2 =  input().rstrip(), input().rstrip()
+    v1, v2 = abs(ord(s1[0])-ord(s2[0])), abs(int(s1[1])-int(s2[1]))
+    if v1 < v2:
+        print(v1, v2)
+    else:
+        print(v2, v1)
+    
