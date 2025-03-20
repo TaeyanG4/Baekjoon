dccea2bdfad90bed111777e67b93157e30ee432d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-03-20 13:30:14 +0900

    [Bronze III] Title: Baby Bites, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/16316. Baby Bites/Baby Bites.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    cnt = 1
+    n = int(input())
+    lst = input().rstrip().split()
+    for i in range(1, n+1):
+        if lst[i-1] == 'mumble' or lst[i-1] == str(i):
+            cnt += 1
+        else:
+            print('something is fishy')
+            break
+    else:
+        print('makes sense')
