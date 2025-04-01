08e26f1652ff1527c6b02b8abd2768fabcf72f69
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-04-02 05:47:29 +0900

    [Bronze III] Title: Elder, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/17042. Elder/Elder.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    alpha = input().rstrip()
+    n = int(input())
+    cnt = set()
+    cnt.add(alpha)
+    for i in range(n):
+        z1, z2 = S()
+        if z2 == alpha:
+            alpha = z1
+            cnt.add(z1)
+    print(alpha)
+    print(len(cnt))
