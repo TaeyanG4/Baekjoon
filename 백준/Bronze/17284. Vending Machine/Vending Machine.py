60bd10ca3b25eef514582e1287978ace96458aff
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-04-08 06:24:37 +0900

    [Bronze III] Title: Vending Machine, Time: 36 ms, Memory: 34668 KB -BaekjoonHub

---
+++ 백준/Bronze/17284. Vending Machine/Vending Machine.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    n = 5000
+    for i in S():
+        if i == 1:
+            n = n - 500
+        elif i == 2:
+            n = n - 800
+        elif i == 3:
+            n = n - 1000
+    print(n)
