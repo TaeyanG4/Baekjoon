be84b85469e172b70e1945d69cc37e8eec558b9e
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-04-24 09:26:13 +0900

    [Bronze III] Title: Jazz Enthusiast, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/18063. Jazz Enthusiast/Jazz Enthusiast.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n, c = S()
+    sec = 0
+    for i in range(n):
+        mm, ss = map(int, input().split(':'))
+        sec += mm * 60 + ss
+    sec -= c*(n-1)
+    print(f"{sec//3600:02}:{(sec%3600)//60:02}:{sec%60:02}")
