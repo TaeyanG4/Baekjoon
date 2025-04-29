8ea417b0ffa164611b222f7a169f12d43e787aec
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-04-29 11:06:43 +0900

    [Bronze III] Title: 피드백, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/18856. 피드백/피드백.py
@@ -0,0 +1,49 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    su = [1]
+    cnt = 2
+    while True:
+        if len(su) == n:
+            print(n)
+            print(*su)
+            break
+        for i in range(2, int(math.sqrt(cnt)) + 1):
+            if cnt % i == 0:
+                break
+        else:
+            su.append(cnt)
+        cnt += 1
