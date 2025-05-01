f5f5ce9bb984e18b9f531a1e057655de1a91e6d9
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-05-01 11:40:23 +0900

    [Bronze III] Title: 가뭄(Small), Time: 36 ms, Memory: 34668 KB -BaekjoonHub

---
+++ 백준/Bronze/19572. 가뭄（Small）/가뭄（Small）.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    ab, ac, bc = S()
+    abc = (ab+ac+bc)/2
+    a, b, c = abc-bc, abc-ac, abc-ab
+    if a < 1 or b < 1 or c < 1:
+        print(-1)
+    else:
+        print(1)
+        print(a, b, c)
