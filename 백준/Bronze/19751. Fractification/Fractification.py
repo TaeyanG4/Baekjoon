98f7bce723e7925f334e3965e42fd7eee7c9b057
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-05-02 11:59:27 +0900

    [Bronze III] Title: Fractification, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/19751. Fractification/Fractification.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    lst = [*S()]
+    tmp = INF
+    ans = []
+    for a, b, c, d in permutations(lst):
+        if tmp > (a/b)+(c/d):
+            ans = [a, b, c, d]
+            tmp = (a/b)+(c/d)
+    print(*ans)
