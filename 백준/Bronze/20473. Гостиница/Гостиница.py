9cbce4e945de69e5078b8896602b41c0a853509d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-05-20 06:56:18 +0900

    [Bronze III] Title: Гостиница, Time: 64 ms, Memory: 40520 KB -BaekjoonHub

---
+++ 백준/Bronze/20473. Гостиница/Гостиница.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    if n % 3 == 0:
+        print(0, n//3)
+    elif n % 3 == 1:
+        print(2, math.ceil(n/3) - 2)
+    else:
+        print(1, math.ceil(n/3) - 1)
