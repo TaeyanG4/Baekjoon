4768be9029641cc84ce7276cd9250e0903869e9d
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-05-24 14:11:20 +0900

    [Bronze III] Title: Statistics, Time: 52 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/20674. Statistics/Statistics.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    ans = 0
+    mi = INF
+    for i in range(int(input())):
+        n = int(input())
+        if n > mi:
+            ans += n - mi
+        else:
+            mi = n
+    print(ans)
