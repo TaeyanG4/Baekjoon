dc90cee23a698c63517bade4ad47eadbe87e2713
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-05-25 10:55:00 +0900

    [Bronze III] Title: Minigolf, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/20877. Minigolf/Minigolf.py
@@ -0,0 +1,48 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    lst = [*S()]
+    sm, pa = 0, 0
+    for i, v in enumerate(lst):
+        if v > 7:
+            sm += 7
+        else:
+            sm += v
+        if i % 2 == 0:
+            pa += 2
+        else:
+            pa += 3
+    print(sm - pa)  
