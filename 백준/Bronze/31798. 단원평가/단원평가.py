a9698bf7dbd46cfc3310169c97a157bef2c80bd2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-07-01 10:10:19 +0900

    [Bronze III] Title: 단원평가, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/31798. 단원평가/단원평가.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    a, b, ab2 = S()
+    if a == 0:
+        print(int(ab2**2-b))
+    elif b == 0:
+        print(int(ab2**2-a))
+    else:
+        print(int((a+b)**0.5))
