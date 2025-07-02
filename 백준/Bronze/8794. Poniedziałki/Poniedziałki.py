105b3c5cc96e29ed1cea7b27063526ba3469d782
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-07-02 13:36:00 +0900

    [Bronze III] Title: Poniedziałki, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/8794. Poniedziałki/Poniedziałki.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    t = int(input())
+    for i in range(t):
+        n, m, l = S()
+        if l != 1:
+            n = n - (m-l+1)
+        d, e = divmod(n, m)
+        print(d if e == 0 else d+1)
