06f18f1fcceacabad4b2f52015de0f2983817377
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-07-05 12:16:35 +0900

    [Bronze III] Title: カレー作り, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/22396. カレー作り/カレー作り.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    while True:
+        r0, w0, c, r = S()
+        if all([r0, w0, c, r]) == 0:
+            break
+        rw = r0/w0
+        if rw >= c:
+            print(0)
+        else:
+            print(math.ceil((c*w0 - r0)/r))
