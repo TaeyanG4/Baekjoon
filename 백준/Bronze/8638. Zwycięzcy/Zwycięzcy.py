e22dfa6c300a69b13f45551d0c4376e2602d8465
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-07-22 06:07:44 +0900

    [Bronze III] Title: Zwycięzcy, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/8638. Zwycięzcy/Zwycięzcy.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    lst = [*S()]
+    mx = max(lst)
+    mx_lst_index = [i for i in range(n) if lst[i] == mx]
+    for i in mx_lst_index:
+        print(chr(i+65), end='')
