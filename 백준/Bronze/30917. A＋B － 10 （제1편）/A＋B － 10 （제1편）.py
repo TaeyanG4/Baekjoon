57eae3fd133767c94e30e8d6b0fdba13412faee6
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-07-31 05:27:00 +0900

    [Bronze III] Title: A+B - 10 (제1편), Time: 4 ms, Memory: 13676 KB -BaekjoonHub

---
+++ 백준/Bronze/30917. A＋B － 10 （제1편）/A＋B － 10 （제1편）.py
@@ -0,0 +1,51 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    for i in range(1, 19):
+        print("? A", i, flush=True)
+        res = int(input())
+        if res == 1:
+            a = i
+            break
+
+    for i in range(1, 19):
+        print("? B", i, flush=True)
+        res = int(input())
+        if res == 1:
+            b = i
+            break
+
+    print("!", a+b)
