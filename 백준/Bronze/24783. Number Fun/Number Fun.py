ea30c09328550d2e303c96bd74b835280bab0ee8
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-08-17 13:50:18 +0900

    [Bronze III] Title: Number Fun, Time: 56 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/24783. Number Fun/Number Fun.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    for i in range(n):
+        a, b, c = S()
+        add, sub, mul, div, sub2, div2 = a + b, a - b, a * b, a / b, b - a, b / a
+        if add == c or sub == c or mul == c or div == c or sub2 == c or div2 == c:
+            print("Possible")
+        else:
+            print("Impossible")
