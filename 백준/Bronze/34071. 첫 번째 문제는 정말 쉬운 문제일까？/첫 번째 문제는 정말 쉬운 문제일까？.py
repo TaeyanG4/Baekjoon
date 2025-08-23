707b974b1621bfcefc63d42b97c456f92626e230
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-08-24 07:59:37 +0900

    [Bronze IV] Title: 첫 번째 문제는 정말 쉬운 문제일까?, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/34071. 첫 번째 문제는 정말 쉬운 문제일까？/첫 번째 문제는 정말 쉬운 문제일까？.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    lst = []
+    for i in range(int(input())):
+        lst.append(int(input()))
+    mi, mx = min(lst), max(lst)
+    if lst[0] == mi:
+        print("ez")
+    elif lst[0] == mx:
+        print("hard")
+    else:
+        print("?")
