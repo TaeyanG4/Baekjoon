9851d624716a43420f98d0127fa7ccf2a3baf9a1
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-08-25 06:12:48 +0900

    [Bronze III] Title: Два станка, Time: 32 ms, Memory: 34536 KB, Score: 14 point -BaekjoonHub

---
+++ 백준/Bronze/24867. Два станка/Два станка.py
@@ -0,0 +1,41 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    k = int(input())
+    a, x = S()
+    b, y = S()
+    tmp1 = (k-a)*x + (k-a-b)*y
+    tmp2 = (k-b)*y + (k-a-b)*x
+    print(max(tmp1, tmp2))
