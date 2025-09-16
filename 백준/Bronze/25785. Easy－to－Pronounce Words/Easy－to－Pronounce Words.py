fa145b7e5fe7000691653d84d70fa50cd5eb7ca4
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-09-17 06:15:07 +0900

    [Bronze III] Title: Easy-to-Pronounce Words, Time: 36 ms, Memory: 36720 KB -BaekjoonHub

---
+++ 백준/Bronze/25785. Easy－to－Pronounce Words/Easy－to－Pronounce Words.py
@@ -0,0 +1,61 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    s = input().strip()
+    odd, even = [], []
+    if s[0] in 'aeiou':
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] not in 'aeiou':
+                    print(0)
+                    break
+            else:
+                if s[i] not in 'bcdfghjklmnpqrstvwxyz':
+                    print(0)
+                    break
+        else:
+            print(1)
+    else:
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] not in 'bcdfghjklmnpqrstvwxyz':
+                    print(0)
+                    break
+            else:
+                if s[i] not in 'aeiou':
+                    print(0)
+                    break
+        else:
+            print(1)
