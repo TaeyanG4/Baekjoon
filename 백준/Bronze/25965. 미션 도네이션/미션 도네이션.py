55962abb68c5d0d480b36be6be9cd00665f17650
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-10-14 06:08:18 +0900

    [Bronze III] Title: 미션 도네이션, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/25965. 미션 도네이션/미션 도네이션.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    for i in range(int(input())):
+        ans = 0
+        kda = []
+        for j in range(int(input())):
+            k, d, a = S()
+            kda.append([k, d, a])
+        kk, dd, aa = S()
+        for k, d, a in kda:
+            ans += kk*k - dd*d + aa*a if kk*k - dd*d + aa*a > 0 else 0
+        print(ans)
+        
