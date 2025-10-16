dada81cb04093b7bb255ec8dd0101bb9674770e2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-10-17 06:02:56 +0900

    [Bronze III] Title: Coffee Cup Combo, Time: 72 ms, Memory: 35320 KB -BaekjoonHub

---
+++ 백준/Bronze/26026. Coffee Cup Combo/Coffee Cup Combo.py
@@ -0,0 +1,48 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    s = input().rstrip()
+    lst = [0] * n
+    for i in range(n):
+        if s[i] == "1":
+            lst[i] = 1
+            if i+1 < n:
+                lst[i+1] = 1
+            if i+2 < n:
+                lst[i+2] = 1
+    print(lst.count(1))
+                
+            
