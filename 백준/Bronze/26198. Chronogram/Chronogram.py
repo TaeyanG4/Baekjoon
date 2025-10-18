d25f495d94390a95467519936a6bafefb5b018d7
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-10-19 06:10:37 +0900

    [Bronze III] Title: Chronogram, Time: 256 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/26198. Chronogram/Chronogram.py
@@ -0,0 +1,57 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+    for i in range(int(input())):
+        ans = 0
+        s = input().rstrip()
+        for i in s:
+            if i == 'I':
+                ans += 1
+            elif i == 'V':
+                ans += 5
+            elif i == 'X':
+                ans += 10
+            elif i == 'L':
+                ans += 50
+            elif i == 'C':
+                ans += 100
+            elif i == 'D':
+                ans += 500
+            elif i == 'M':
+                ans += 1000
+        print(ans)
+
+            
