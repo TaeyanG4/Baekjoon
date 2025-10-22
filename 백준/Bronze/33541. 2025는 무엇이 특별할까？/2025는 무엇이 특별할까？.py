ffa72fd771371df8119e0ccb8f1c385c0e782f00
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-10-23 06:08:24 +0900

    [Bronze III] Title: 2025는 무엇이 특별할까?, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/33541. 2025는 무엇이 특별할까？/2025는 무엇이 특별할까？.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(str, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    s = input().rstrip()
+    while True:
+        s = str(int(s) + 1)
+        if int(s) == (int(s[0:2]) + int(s[2:4])) ** 2:
+            print(s)
+            break
+        if int(s) > 9999:
+            print(-1)
+            break
