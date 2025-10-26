fe35805c3b8de848760eb840c03a23fd319954df
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-10-27 06:05:18 +0900

    [Bronze III] Title: Desni klik, Time: 36 ms, Memory: 34536 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/26392. Desni klik/Desni klik.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n, r, s = S()
+    for i in range(n):
+        board = [input().strip() for _ in range(r)]
+        cnt = 0
+        mi, mx = INF, -INF
+        for line in board:
+            if '#' in line:
+                mi = min(mi, cnt)
+                mx = max(mx, cnt)
+            cnt += 1
+        print(mx-mi)
