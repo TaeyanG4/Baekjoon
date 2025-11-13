fdaa9c1326477781ca2a5c4417e78cfdd4e377c1
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-11-14 08:29:48 +0900

    [Bronze IV] Title: 할인이 필요해, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/34323. 할인이 필요해/할인이 필요해.py
@@ -0,0 +1,39 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n, m, a = S()
+    p1 = m * a
+    p2 = (m + 1) * a * (100 - n) // 100
+    print(min(p1, p2))
