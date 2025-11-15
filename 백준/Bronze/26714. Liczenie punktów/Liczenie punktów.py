fdfb1511f50e21458be70bf92144ff5cb33cd4a5
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-11-15 09:33:51 +0900

    [Bronze III] Title: Liczenie punktów, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/26714. Liczenie punktów/Liczenie punktów.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    s = input().rstrip()
+    ans = 0
+    group_size = n//10
+    for i in range(10):
+        start = i * group_size
+        end = (i + 1) * group_size
+        if 'N' not in s[start:end]:
+            ans += 1
+    print(ans)
