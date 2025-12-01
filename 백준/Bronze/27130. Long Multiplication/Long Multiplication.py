1275a2140dd69a571f3eca6d684d1a5157cec4d7
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-12-01 09:33:13 +0900

    [Bronze III] Title: Long Multiplication, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/27130. Long Multiplication/Long Multiplication.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    lst1 = int(input())
+    lst2 = int(input())
+    print(lst1)
+    print(lst2)
+    for i in str(lst2)[::-1]:
+        print(lst1*int(i))
+    print(lst1*lst2)
