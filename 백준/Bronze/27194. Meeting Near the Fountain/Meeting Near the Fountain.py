f4364cd99d06be8fe0f3e61143f36085c71ce9c2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-12-02 10:00:16 +0900

    [Bronze III] Title: Meeting Near the Fountain, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/27194. Meeting Near the Fountain/Meeting Near the Fountain.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n, t = S()
+    m = int(input())
+    x, y = S()
+    tmp = m/x + (n-m)/y - (t*60)
+    if tmp <= 0:
+        print(0)
+    else:
+        print(math.ceil(tmp/60))
