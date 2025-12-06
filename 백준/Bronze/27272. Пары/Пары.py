aa4ae326501834815de2aec42569151ec90a75dd
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-12-06 14:46:45 +0900

    [Bronze III] Title: Пары, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/27272. Пары/Пары.py
@@ -0,0 +1,40 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    a, b, c, d = S()
+    ab_cd = a*b+c*d
+    ac_bd = a*c+b*d
+    ad_bc = a*d+b*c
+    print(max(ab_cd, ac_bd, ad_bc))
