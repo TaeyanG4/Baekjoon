15db2f9531ecf7f70fd4c883671943fb82ad29b2
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2025-12-11 06:23:12 +0900

    [Bronze III] Title: JOI エディタ (JOI Editor), Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/27333. JOI エディタ （JOI Editor）/JOI エディタ （JOI Editor）.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    n = int(input())
+    s = list(input().rstrip())
+    for i in range(n-1):
+        if s[i].islower() and s[i+1].islower() and s[i] == s[i+1]:
+            s[i] = s[i].upper()
+            s[i+1] = s[i+1].upper()
+    print("".join(s))
+            
