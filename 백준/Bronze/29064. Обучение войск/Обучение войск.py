8f34205ec6c4dbe9d2b10dd530ce81d987e6c465
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-01-03 08:53:03 +0900

    [Bronze III] Title: Обучение войск, Time: 48 ms, Memory: 36104 KB -BaekjoonHub

---
+++ 백준/Bronze/29064. Обучение войск/Обучение войск.py
@@ -0,0 +1,43 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    
+    n = int(input())
+    lst = [*S()] 
+    num_1 = lst.count(1)
+    tmp = math.ceil(n/2)
+    if tmp <= num_1:
+        print(0)
+    else:
+        print(tmp-num_1)
