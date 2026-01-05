00a6ba2c0f5dc58e1b76bb46ac4546be0a6301f0
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-01-05 11:09:54 +0900

    [Bronze III] Title: Фома и занимательная математика, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/29133. Фома и занимательная математика/Фома и занимательная математика.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    
+    a, b, c, d = S()
+    ans = []
+    for x in range(1, 4):
+        tmp = a**x + b**x + c**x
+        if tmp == d:
+            ans.append(x)
+
+    if len(ans) == 1:
+        print(ans[0])
+    else:
+        print(-1)
