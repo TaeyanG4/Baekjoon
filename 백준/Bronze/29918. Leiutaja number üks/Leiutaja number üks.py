5b29c9078b2ebcadc400cf65869436d12e43443e
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-01-21 13:27:59 +0900

    [Bronze III] Title: Leiutaja number üks, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/29918. Leiutaja number üks/Leiutaja number üks.py
@@ -0,0 +1,47 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n, m = S()
+    a = n+m*10
+    mx = 0
+    for i in range(5):
+        nn, mm = S()
+        b = nn+mm*10
+        mx = max(mx, b)
+    if a > mx:
+        print(0)
+    else:
+        print(mx-a+1)
+        
