906f71aa79a740fb03a569c6d7caf05e8266ec4f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-01-23 06:03:12 +0900

    [Bronze III] Title: Age Expression, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/30215. Age Expression/Age Expression.py
@@ -0,0 +1,51 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    o, a, k = S()
+    aa, kk = 1, 1
+    while True:
+        if aa > o or kk > o:
+            print(0)
+            break
+        while True:
+            tmp = (a*aa)+(k*kk)
+            if o == tmp:
+                print(1)
+                exit()
+            elif o < tmp:
+                break
+            aa += 1
+        kk += 1
+        aa = 1
