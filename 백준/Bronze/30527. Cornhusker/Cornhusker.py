cdb27709ad95d29ff2ba504efbcc5723e1c19398
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-02-05 12:16:14 +0900

    [Bronze III] Title: Cornhusker, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/30527. Cornhusker/Cornhusker.py
@@ -0,0 +1,41 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    lst = [*S()]
+    n, k = S()
+    tmp = 0
+    for i in range(0, len(lst), 2):
+        tmp += lst[i] * lst[i+1]
+    print(int((tmp//5)*n // k))
