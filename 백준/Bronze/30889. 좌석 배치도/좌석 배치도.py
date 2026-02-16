2a2f363f7df7476b4703f043decf4df6ab434d7f
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-02-16 09:21:03 +0900

    [Bronze III] Title: 좌석 배치도, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/30889. 좌석 배치도/좌석 배치도.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    board = [['.' for _ in range(20)] for _ in range(10)]
+    n = int(input())
+    for i in range(n):
+        s = input().strip()
+        r, c = ord(s[0])-65, int(s[1:])-1
+        board[r][c] = 'o'
+    
+    for line in board:
+        print(''.join(line))
