ae662690117be130951789550f6cbf2796895c60
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-02-17 09:25:40 +0900

    [Bronze III] Title: 빅데이터 vs 정보보호 vs 인공지능, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/30957. 빅데이터 vs 정보보호 vs 인공지능/빅데이터 vs 정보보호 vs 인공지능.py
@@ -0,0 +1,48 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    s = input().rstrip()
+    B, S, A = s.count('B'), s.count('S'), s.count('A')
+    mx = max(B, S, A)
+    if B == S == A:
+        print('SCU')
+    else:
+        if mx == B:
+            print('B', end = '')
+        if mx == S:
+            print('S', end = '')
+        if mx == A:
+            print('A', end = '')
