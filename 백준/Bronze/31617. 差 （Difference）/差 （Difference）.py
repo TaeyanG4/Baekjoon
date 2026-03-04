7976c88e1a6134c030c61c239251553b9f5eb624
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-03-04 10:10:52 +0900

    [Bronze III] Title: 差 (Difference), Time: 36 ms, Memory: 35560 KB -BaekjoonHub

---
+++ 백준/Bronze/31617. 差 （Difference）/差 （Difference）.py
@@ -0,0 +1,46 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    
+    k = int(input())
+    n = int(input())
+    lst_a = [*S()]
+    m = int(input())
+    lst_b = [*S()]
+    lst_comb = list(product(lst_a, lst_b))
+    ans = 0
+    for i, j in lst_comb:
+        if i + k == j:
+            ans += 1
+    print(ans)
