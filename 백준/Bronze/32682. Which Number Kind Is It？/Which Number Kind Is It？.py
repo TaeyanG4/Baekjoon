fd65047e40bedf02a5deaf05e1d869889af1d392
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-03-28 11:06:33 +0900

    [Bronze III] Title: Which Number Kind Is It?, Time: 152 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.py
@@ -0,0 +1,44 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    for i in range(int(input())):
+        t = int(input())
+        ans = []
+        if t % 2 != 0:
+            ans.append("O")
+        if math.sqrt(t) == int(math.sqrt(t)):
+            ans.append("S")
+        if not ans:
+            ans.append("EMPTY")
+        print("".join(ans))
