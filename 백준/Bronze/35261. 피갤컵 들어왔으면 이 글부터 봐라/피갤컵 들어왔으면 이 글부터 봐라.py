c25ff968f39e2661a5a266d4c039f3335f296654
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-04-08 12:57:50 +0900

    [Bronze III] Title: 피갤컵 들어왔으면 이 글부터 봐라, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/35261. 피갤컵 들어왔으면 이 글부터 봐라/피갤컵 들어왔으면 이 글부터 봐라.py
@@ -0,0 +1,45 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n = int(input())
+    s = input().rstrip()
+    mx = 0
+    for i in range(n-4):
+        tmp = 0
+        for j, k in zip(s[i:i+5], 'eagle'):
+            if j == k:
+                tmp += 1
+        mx = max(mx, tmp)
+    print(5-mx)
+    
