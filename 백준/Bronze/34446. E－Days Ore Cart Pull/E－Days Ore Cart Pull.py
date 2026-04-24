37efac7e6cee6a56aaed07c0b489f5ee20c649af
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-04-24 10:29:36 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.py
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.py
@@ -1,36 +1,12 @@
-## taeyang's template (1.0.8)
-#################################
-## my import lines
-import sys
-import math
-# import copy
-# import ast
-# import re
-# import time
-# import json
-# import pprint
-# from collections import *
-# from heapq import *
-# from queue import PriorityQueue
-# from itertools import *
-# from statistics import *
-# from datetime import *
-# from bisect import *
-# from fractions import Fraction
-# from decimal import *
-# getcontext().prec = 20
-#################################
-
-def solution():
-    pass
-
-if __name__ == "__main__":
-    input = sys.stdin.readline
-    S = lambda: map(int, input().split())
-    INF = float('inf')
-    # MOD = 10**9 + 7
-    # sys.setrecursionlimit(10**6)
-    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    
-    a, b, c = int(input()), int(input()), int(input())
-    print(a*2)
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
