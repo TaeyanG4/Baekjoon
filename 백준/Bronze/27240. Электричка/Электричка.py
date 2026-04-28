d41a5791d76d4fc0fb8d3d822f2a0a87ae4a0328
Taeyang <89699036+TaeyanG4@users.noreply.github.com>
2026-04-28 13:33:07 +0900

    [Bronze IV] Title: Электричка, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/27240. Электричка/Электричка.py
@@ -0,0 +1,42 @@
+## taeyang's template (1.0.8)
+#################################
+## my import lines
+import sys
+import math
+# import copy
+# import ast
+# import re
+# import time
+# import json
+# import pprint
+# from collections import *
+# from heapq import *
+# from queue import PriorityQueue
+# from itertools import *
+# from statistics import *
+# from datetime import *
+# from bisect import *
+# from fractions import Fraction
+# from decimal import *
+# getcontext().prec = 20
+#################################
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    S = lambda: map(int, input().split())
+    INF = float('inf')
+    # MOD = 10**9 + 7
+    # sys.setrecursionlimit(10**6)
+    # direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    n, a, b = S()
+    s, t = S()
+    if (s <= a and t <= a) or (s >= b and t >= b):
+        print("Outside")
+    elif a < s < b  and a < t < b:
+        print("City")
+    else:
+        print("Full")
